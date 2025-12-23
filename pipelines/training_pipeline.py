d292509f3bd472909fde07b3896da62c4099a667
Naitik Pal <naitikpal513@gmail.com>
2025-12-23 16:42:33 +0530

    Add pipeline File

---
+++ pipelines/training_pipeline.py
@@ -0,0 +1,6 @@
+## ZenML is tool that helps you organize your machine learning 
+## work properly like a real system instead of a notebook.
+
+## MLFlow is a tool that remembers everything you try im ML.
+## It tracks ML experiments parameters metrics and models unlike 
+## traditional notebook approach 
